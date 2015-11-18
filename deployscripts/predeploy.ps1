4e192f9ccc7e82cd14d0f6e70b64ef98e8f492e5
Jasper Nygaard <jasper@sovs.net>
2015-11-18 14:48:18 +0100

    Use octopus variables to parameterize predeploy.ps1

--- deployscripts/predeploy.ps1
+++ deployscripts/predeploy.ps1
@@ -1,12 +1,11 @@
  #http://www.red-gate.com/products/oracle-development/dlm-automation-suite-for-oracle/walkthrough
 
-param(
-    [Parameter(Mandatory=$true)]
-    [string]$password=$OctopusParameters["password"])
+$password=$OctopusParameters["dbPassword"]
+$host = $OctopusParameters["dbServer"]
 
 $ErrorActionPreference = "Stop"
 $here = Split-Path $MyInvocation.MyCommand.Definition
 
 $ScoExe = ".\tools\SCO\SCO.exe"
 
-& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@dev-ora-db.hyperv.lc.skejby/XE{HR}" /scriptfile="updatescript.sql" /report:"predeploy.html"
+& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$host/XE{HR}" /scriptfile="updatescript.sql" /report:"predeploy.html"
