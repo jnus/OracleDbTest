3d8a7681ac1f78c9c5b8fa765f4d0091b10b36d1
Jasper Nygaard <jasper@sovs.net>
2015-11-18 15:02:35 +0100

    commit

--- deployscripts/predeploy.ps1
+++ deployscripts/predeploy.ps1
@@ -2,6 +2,7 @@
 
 $password=$OctopusParameters["dbPassword"]
 $dbHost = $OctopusParameters["dbServer"]
+$OctopusEnvironment = $OctopusParameters["Environment"]
 
 $ErrorActionPreference = "Stop"
 $here = Split-Path $MyInvocation.MyCommand.Definition
@@ -9,3 +10,8 @@ $here = Split-Path $MyInvocation.MyCommand.Definition
 $ScoExe = ".\tools\SCO\SCO.exe"
 
 & $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$dbHost/XE{HR}" /scriptfile="updatescript.sql" /report:"predeploy.html"
+
+New-OctopusArtifact -Path ".\predeploy.html" -Name "predeploy-$Environment.html"
+New-OctopusArtifact -Path ".\updatescript.sql" -Name "updatescript-$Environment.sql"
+
+exit 0
