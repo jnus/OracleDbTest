eaf1fe6f6936326538e4743e20ca3d9380821b28
Jasper Nygaard <jasper@sovs.net>
2015-11-18 14:53:49 +0100

    commit

--- deployscripts/predeploy.ps1
+++ deployscripts/predeploy.ps1
@@ -1,11 +1,11 @@
  #http://www.red-gate.com/products/oracle-development/dlm-automation-suite-for-oracle/walkthrough
 
 $password=$OctopusParameters["dbPassword"]
-$host = $OctopusParameters["dbServer"]
+$dbHost = $OctopusParameters["dbServer"]
 
 $ErrorActionPreference = "Stop"
 $here = Split-Path $MyInvocation.MyCommand.Definition
 
 $ScoExe = ".\tools\SCO\SCO.exe"
 
-& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$host/XE{HR}" /scriptfile="updatescript.sql" /report:"predeploy.html"
+& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$dbHost/XE{HR}" /scriptfile="updatescript.sql" /report:"predeploy.html"
