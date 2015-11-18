21c5f0345e82af1ebecd57a8701d7c64cd0f835b
Jasper Nygaard <jasper@sovs.net>
2015-11-18 14:31:22 +0100

    Predeploy.ps1 compares source and initial dest. Create diff report and update script

--- deployscripts/predeploy.ps1
+++ deployscripts/predeploy.ps1
@@ -1,1 +1,12 @@
-Write-Host "PreDeploy!!"
+ #http://www.red-gate.com/products/oracle-development/dlm-automation-suite-for-oracle/walkthrough
+
+param(
+    [Parameter(Mandatory=$true)]
+    [string]$password)
+
+$ErrorActionPreference = "Stop"
+$here = Split-Path $MyInvocation.MyCommand.Definition
+
+$ScoExe = ".\tools\SCO\SCO.exe"
+
+& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@dev-ora-db.hyperv.lc.skejby/XE{HR}" /scriptfile="updatescript.sql" /report:"predeploy.html"
