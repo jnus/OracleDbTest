9fa5adf3bd1a286ad3183a617943f21f4409988f
Jasper Nygaard <jasper@sovs.net>
2015-11-19 09:07:30 +0100

    commit

--- deployscripts/deploy.ps1
+++ deployscripts/deploy.ps1
@@ -5,20 +5,21 @@ $password=$OctopusParameters["dbPassword"]
 $dbHost = $OctopusParameters["dbServer"]
 $OctopusEnvironment = $OctopusParameters["Environment"]
 $OctopusReleaseVersion = $OctopusParameters["Version"]
-
+$ScriptFilename = "$OctopusEnvironment.$OctopusReleaseVersion.script.sql"
+$ReportFilename = "$OctopusEnvironment.$OctopusReleaseVersion.report.html"
 $here = Split-Path $MyInvocation.MyCommand.Definition
 
 $ScoExe = ".\tools\SCO\SCO.exe"
 
 # Deploy changes using Redgate Schema Compare for Oracle
 Write-Warning "Upgrading HR"
-& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$dbHost/XE{HR}" -deploy /scriptfile="script.sql" /report:"deploy.html"
-
+& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$dbHost/XE{HR}" -deploy /scriptfile=$ScriptFilename /report:$ReportFilename
 Write-Output "Schema Compare for Oracle exited with code $lastExitCode"
 $ScoExitCode = $lastExitCode
 
-New-OctopusArtifact -Path ".\deploy.html" -Name "deploy.$OctopusEnvironment.$OctopusReleaseVersion.html"
-New-OctopusArtifact -Path ".\script.sql" -Name "script.$OctopusEnvironment.$OctopusReleaseVersion.sql"
+New-OctopusArtifact -Path ".\$ScriptFilename" -Name $ScriptFilename
+New-OctopusArtifact -Path ".\$ReportFilename" -Name $ReportFilename
+
 
 # Exit code 61 is simply telling us there are differences that have been deployed.
 if( $ScoExitCode -eq 61)
--- deployscripts/predeploy.ps1
+++ deployscripts/predeploy.ps1
@@ -5,18 +5,20 @@ $password=$OctopusParameters["dbPassword"]
 $dbHost = $OctopusParameters["dbServer"]
 $OctopusEnvironment = $OctopusParameters["Environment"]
 $OctopusReleaseVersion = $OctopusParameters["Version"]
+$ScriptFilename = "$OctopusEnvironment.$OctopusReleaseVersion.script.sql"
+$ReportFilename = "$OctopusEnvironment.$OctopusReleaseVersion.report.html"
 
 $here = Split-Path $MyInvocation.MyCommand.Definition
 
 $ScoExe = ".\tools\SCO\SCO.exe"
 
-& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$dbHost/XE{HR}" /scriptfile="prescript.sql" /report:"predeploy.html"
+& $ScoExe /source ".\dbscripts{HR}" /target "System/$password@$dbHost/XE{HR}" /scriptfile=$ScriptFilename /report:$ReportFilename
 
 Write-Output "Schema Compare for Oracle exited with code $lastExitCode"
 $ScoExitCode = $lastExitCode
 
-New-OctopusArtifact -Path ".\predeploy.html" -Name "predeploy.$OctopusEnvironment.$OctopusReleaseVersion.html"
-New-OctopusArtifact -Path ".\prescript.sql" -Name "prescript.$OctopusEnvironment.$OctopusReleaseVersion.sql"
+New-OctopusArtifact -Path ".\$ScriptFilename" -Name $ScriptFilename
+New-OctopusArtifact -Path ".\$ReportFilename" -Name $ReportFilename
 
 # Exit code 61 is simply telling us there are differences that have been deployed.
 if( $ScoExitCode -eq 61)
